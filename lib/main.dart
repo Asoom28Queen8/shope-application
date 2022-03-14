5a3ba572965315924421f6d526e498d75e54e0c7
Asoom28Queen8 <>
2022-03-14 21:37:06 +0300

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shope_application/screens/product_detail_screen.dart';
 import 'package:shope_application/screens/product_overview_screen.dart';
 
 void main() {
@@ -9,12 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shope',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: product_overview_screen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Shope',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+        home: product_overview_screen(),
+        routes: {
+          product_detail_screen.routeName:(_) =>product_detail_screen() ,
+          
+        },
+        );
   }
 }
---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class product_detail_screen  extends StatelessWidget {
+      static const routeName='/product_detail';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
