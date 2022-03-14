d629fbbfa050852172c6741b68eef419c5b0eec0
Asoom28Queen8 <>
2022-03-14 19:05:58 +0300

    start in product overview screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:shope_application/screens/product_overview_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shope',
       theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'Home page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
- 
-  @override
-  Widget build(BuildContext context) {
-  
-    return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-      ),
-      body: Center(),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed:,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
+      home: product_overview_screen(),
     );
   }
 }
---
+++ lib/screens/product_overview_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class product_overview_screen extends StatefulWidget {
+  const product_overview_screen({ Key? key }) : super(key: key);
+
+  @override
+  State<product_overview_screen> createState() => _product_overview_screenState();
+}
+
+class _product_overview_screenState extends State<product_overview_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(),
+    );
+  }
+}
